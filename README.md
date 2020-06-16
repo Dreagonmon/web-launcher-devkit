55718442f20462a007a0839254a445e7be833520
Dreagonmon <531486058@qq.com>
2020-06-16 13:10:57 +0800

    add dev.js

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # web-launcher-devkit
 some deno script to development Android Launcher
+
+## start development server
+run dev server:
+
+```bash
+deno run --allow-net https://dreagonmon.github.io/web-launcher-devkit/scripts/dev_server.js -p 10801
+```
+
+run file server:
+
+```bash
+deno run --allow-net --allow-read https://dreagonmon.github.io/web-launcher-devkit/scripts/file_server.js -p 1080
+```
+
+run both with default port
+
+```bash
+deno run --allow-run --allow-read https://dreagonmon.github.io/web-launcher-devkit/scripts/dev.js
+```
---
+++ scripts/dev.js
@@ -0,0 +1,136 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+// This is a specialised implementation of a System module loader.
+
+"use strict";
+
+// @ts-nocheck
+/* eslint-disable */
+let System, __instantiateAsync, __instantiate;
+
+(() => {
+  const r = new Map();
+
+  System = {
+    register(id, d, f) {
+      r.set(id, { d, f, exp: {} });
+    },
+  };
+
+  async function dI(mid, src) {
+    let id = mid.replace(/\.\w+$/i, "");
+    if (id.includes("./")) {
+      const [o, ...ia] = id.split("/").reverse(),
+        [, ...sa] = src.split("/").reverse(),
+        oa = [o];
+      let s = 0,
+        i;
+      while ((i = ia.shift())) {
+        if (i === "..") s++;
+        else if (i === ".") break;
+        else oa.push(i);
+      }
+      if (s < sa.length) oa.push(...sa.slice(s));
+      id = oa.reverse().join("/");
+    }
+    return r.has(id) ? gExpA(id) : import(mid);
+  }
+
+  function gC(id, main) {
+    return {
+      id,
+      import: (m) => dI(m, id),
+      meta: { url: id, main },
+    };
+  }
+
+  function gE(exp) {
+    return (id, v) => {
+      v = typeof id === "string" ? { [id]: v } : id;
+      for (const [id, value] of Object.entries(v)) {
+        Object.defineProperty(exp, id, {
+          value,
+          writable: true,
+          enumerable: true,
+        });
+      }
+    };
+  }
+
+  function rF(main) {
+    for (const [id, m] of r.entries()) {
+      const { f, exp } = m;
+      const { execute: e, setters: s } = f(gE(exp), gC(id, id === main));
+      delete m.f;
+      m.e = e;
+      m.s = s;
+    }
+  }
+
+  async function gExpA(id) {
+    if (!r.has(id)) return;
+    const m = r.get(id);
+    if (m.s) {
+      const { d, e, s } = m;
+      delete m.s;
+      delete m.e;
+      for (let i = 0; i < s.length; i++) s[i](await gExpA(d[i]));
+      const r = e();
+      if (r) await r;
+    }
+    return m.exp;
+  }
+
+  function gExp(id) {
+    if (!r.has(id)) return;
+    const m = r.get(id);
+    if (m.s) {
+      const { d, e, s } = m;
+      delete m.s;
+      delete m.e;
+      for (let i = 0; i < s.length; i++) s[i](gExp(d[i]));
+      e();
+    }
+    return m.exp;
+  }
+
+  __instantiateAsync = async (m) => {
+    System = __instantiateAsync = __instantiate = undefined;
+    rF(m);
+    return gExpA(m);
+  };
+
+  __instantiate = (m) => {
+    System = __instantiateAsync = __instantiate = undefined;
+    rF(m);
+    return gExp(m);
+  };
+})();
+
+"use strict";
+const p1 = Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-net",
+    "https://dreagonmon.github.io/web-launcher-devkit/scripts/dev_server.js",
+    "-p",
+    "10801",
+  ],
+});
+const p2 = Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-net",
+    "--allow-read",
+    "https://dreagonmon.github.io/web-launcher-devkit/scripts/file_server.js",
+    "-p",
+    "1080",
+  ],
+});
+await p1.status();
+await p2.status();
+
+__instantiate("dev");
+
