b50173129d45c5984312b43b90f5ce0369b1dbc7
Dreagonmon <531486058@qq.com>
2020-06-18 13:56:50 +0800

    update launcher.js

--- examples/js/launcher.js
+++ examples/js/launcher.js
@@ -6,19 +6,20 @@ class ApplicationInfo {
   activity = "";
 }
 class LauncherResponseError extends Error {
-  constructor(msg) {
+  constructor (msg) {
     super("LauncherResponseError: " + msg);
   }
 }
 
 // fake server
-class Random{
+class Random {
   seed;
-  constructor(seed = Math.PI*Math.PI){
+  constructor (seed = Math.PI * Math.PI) {
     this.seed = seed;
   };
-  nextInt(min, max) {
-    let rand = Number.parseFloat("0."+Math.sin(this.seed ++).toString().substr(6));
+
+  nextInt (min, max) {
+    const rand = Number.parseFloat("0." + Math.sin(this.seed++).toString().substr(6));
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(rand * (max - min)) + min;
@@ -36,76 +37,79 @@ const COLOR_LIST = [
   // "#795548", "#9e9e9e", "#9e9e9e",
 ];
 const getRandomWord = (random) => {
-  let pos = random.nextInt(0, WORD_LIST.length);
+  const pos = random.nextInt(0, WORD_LIST.length);
   return WORD_LIST[pos];
-}
+};
 const getRandomColor = (random) => {
-  let pos = random.nextInt(0, COLOR_LIST.length);
+  const pos = random.nextInt(0, COLOR_LIST.length);
   return COLOR_LIST[pos];
-}
+};
 const toFirstUpperCase = (text) => {
   return text.replace(/^./u, text.charAt(0).toUpperCase());
-}
-const generateRandomAppList = (seed = Math.PI*Math.PI, size = 50) => {
-  let apps = [];
-  let colors = {};
-  let r = new Random(seed);
-  for (let i=0; i<size; i++){
-    let length = r.nextInt(1, 4); // name word count [1, 4)
-    let app = {name: "", activity: "", package: ""};
-    for (let k=0; k<length; k++){
-      let word = getRandomWord(r);
+};
+const generateRandomAppList = (seed = Math.PI * Math.PI, size = 50) => {
+  const apps = [];
+  const colors = {};
+  const r = new Random(seed);
+  for (let i = 0; i < size; i++) {
+    const length = r.nextInt(1, 4); // name word count [1, 4)
+    const app = { name: "", activity: "", package: "" };
+    for (let k = 0; k < length; k++) {
+      const word = getRandomWord(r);
       app.name += toFirstUpperCase(word) + " ";
       app.package += word + ".";
     }
     app.activity = app.package + "MainActivity";
     app.name = app.name.substr(0, app.name.length - 1);
     app.package = app.package.substr(0, app.package.length - 1);
-    let theme = [getRandomColor(r), getRandomColor(r), getRandomColor(r)];
+    const theme = [getRandomColor(r), getRandomColor(r), getRandomColor(r)];
     colors[app.package] = theme;
     apps.push(app);
   }
   return [apps, colors];
 };
 const [APP_LIST, APP_THEME] = generateRandomAppList();
-class MockServer{
+class MockServer {
   permission = {}; // permission only request once
   hasLaunchPermission = () => {
-    return this.permission["launcher"] ? true : false;
+    return !!this.permission.launcher;
   };
+
   getApplicationList = async () => {
-    if (!this.hasLaunchPermission()){
+    if (!this.hasLaunchPermission()) {
       console.error("launchApplication: You need request permission first!");
-      return Promise.reject();
+      return Promise.reject(new LauncherResponseError("No Permission"));
     }
     return APP_LIST;
   };
+
   getApplicationIconSrc = (packageName, activity, size = ICON_SIZE) => {
     let theme;
-    if (packageName != null){
+    if (packageName != null) {
       theme = APP_THEME[packageName];
-    }else{
-      let r = new Random(Math.random()*Number.MAX_SAFE_INTEGER);
+    } else {
+      const r = new Random(Math.random() * Number.MAX_SAFE_INTEGER);
       theme = [getRandomColor(r), getRandomColor(r), getRandomColor(r)];
     }
-    let SVG_IMAGE = `<svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+    const SVG_IMAGE = `<svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
     <g>
-     <rect x="-1" y="-1" width="${size+2}" height="${size+2}" id="canvas_background" fill="none"/>
+     <rect x="-1" y="-1" width="${size + 2}" height="${size + 2}" id="canvas_background" fill="none"/>
     </g>
     <g>
-     <ellipse ry="${size/2.2}" rx="${size/2.2}" id="svg_3" cy="${size/2}" cx="${size/2}" stroke-width="0" stroke="#000" fill="${theme[0]}"/>
-     <ellipse ry="${size/3}" rx="${size/3}" id="svg_3" cy="${size/2}" cx="${size/2}" stroke-width="0.5" stroke="#000" fill="${theme[1]}"/>
-     <ellipse ry="${size/5}" rx="${size/5}" id="svg_3" cy="${size/2}" cx="${size/2}" stroke-width="0" stroke="#000" fill="${theme[2]}"/>
+     <ellipse ry="${size / 2.2}" rx="${size / 2.2}" id="svg_3" cy="${size / 2}" cx="${size / 2}" stroke-width="0" stroke="#000" fill="${theme[0]}"/>
+     <ellipse ry="${size / 3}" rx="${size / 3}" id="svg_3" cy="${size / 2}" cx="${size / 2}" stroke-width="0.5" stroke="#000" fill="${theme[1]}"/>
+     <ellipse ry="${size / 5}" rx="${size / 5}" id="svg_3" cy="${size / 2}" cx="${size / 2}" stroke-width="0" stroke="#000" fill="${theme[2]}"/>
     </g>
     </svg>`;
-    return "data:image/svg+xml;base64,"+btoa(SVG_IMAGE);
+    return "data:image/svg+xml;base64," + btoa(SVG_IMAGE);
   };
+
   launchApplication = async (packageName, activity) => {
-    if (!this.hasLaunchPermission()){
+    if (!this.hasLaunchPermission()) {
       console.error("launchApplication: You need request permission first!");
-      return Promise.reject();
+      return Promise.reject(new LauncherResponseError("No Permission"));
     }
-    let frame = document.createElement("div");
+    const frame = document.createElement("div");
     frame.style.width = "100%";
     frame.style.height = "100%";
     frame.style.background = "black";
@@ -122,17 +126,17 @@ class MockServer{
       event.stopPropagation();
       document.body.removeChild(frame);
     };
-    let title = document.createElement("h1");
+    const title = document.createElement("h1");
     title.innerText = "Application";
     title.style.color = "white";
     title.style.wordBreak = "break-all";
     frame.appendChild(title);
-    let pkg = document.createElement("h3");
+    const pkg = document.createElement("h3");
     pkg.innerText = packageName;
     pkg.style.color = "white";
     pkg.style.wordBreak = "break-all";
     frame.appendChild(pkg);
-    let pkg2 = document.createElement("h3");
+    const pkg2 = document.createElement("h3");
     pkg2.innerText = activity;
     pkg2.style.color = "white";
     pkg2.style.wordBreak = "break-all";
@@ -140,73 +144,75 @@ class MockServer{
     document.body.appendChild(frame);
     return true;
   };
+
   requestPermission = async (name) => {
     const NAME_MAP = {
-      "launcher": "Launcher",
-    }
-    if (!["launcher"].includes(name)){
+      launcher: "Launcher",
+    };
+    if (!["launcher"].includes(name)) {
       console.error("requestPermission: Permission name not support!");
-      return Promise.reject();
+      return Promise.reject(new LauncherResponseError("Permission name not support"));
     }
-    if (this.permission[name]){
+    if (this.permission[name]) {
       return true;
     }
-    let result = confirm(`Do you want to grant ${NAME_MAP[name]} permission to ${window.location.origin} ?`);
-    if (result){
+    const result = confirm(`Do you want to grant ${NAME_MAP[name]} permission to ${window.location.origin} ?`);
+    if (result) {
       this.permission[name] = true;
       return true;
     }
-    return Promise.reject();
+    return Promise.reject(new LauncherResponseError("Permission denided"));
   }
 }
 
 // launcher api
 const getParameterByName = (url, name) => {
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-      results = regex.exec(url);
+  name = name.replace(/[[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  var results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 class Launcher {
   PERMISSION_LAUNCHER = "launcher";
-  constructor(port = -1) {
+  constructor (port = -1) {
     if (port < 0) {
       // try to get the port
       try {
-        let url = window.location.href;
-        let portText = getParameterByName(url, "port");
+        const url = window.location.href;
+        const portText = getParameterByName(url, "port");
         port = Number.parseInt(portText);
         if (portText == null || port < 0) {
           port = 10801;
         }
-      } catch{
+      } catch {
         console.log("error, default port!", port);
         port = 10801;
       }
       console.log("using port:", port);
     }
     let protocol = window.location.protocol;
-    if (protocol != "http:"){
-      protocol = "https:"
+    if (protocol !== "http:") {
+      protocol = "https:";
     }
     this.port = port;
     this.server = undefined;
     this.address = `${protocol}//127.0.0.1:${port}/api/`;
   };
+
   getApplicationList = async () => {
-    if (this.server){
+    if (this.server) {
       return await this.server.getApplicationList();
     }
-    let url = `${this.address}app-list-all?t=${new Date().getTime()}`;
-    let resp = await fetch(url);
-    if (resp.status != 200) {
+    const url = `${this.address}app-list-all?t=${new Date().getTime()}`;
+    const resp = await fetch(url);
+    if (resp.status !== 200) {
       return Promise.reject(new LauncherResponseError("status code not 200!"));
     }
-    let arrays = JSON.parse(await resp.text());
-    let appList = arrays.map(item => {
-      let app = new ApplicationInfo();
+    const arrays = JSON.parse(await resp.text());
+    const appList = arrays.map(item => {
+      const app = new ApplicationInfo();
       app.name = item.name;
       app.package = item.package;
       app.activity = item.activity;
@@ -214,34 +220,38 @@ class Launcher {
     });
     return appList;
   };
+
   getApplicationIconSrc = (packageName, activity, size = 128) => {
-    if (this.server){
+    if (this.server) {
       return this.server.getApplicationIconSrc(packageName, activity, size);
     }
     return `${this.address}icon?package=${packageName}&activity=${activity}&size=${size}&t=${new Date().getTime()}`;
   };
+
   launchApplication = async (packageName, activity) => {
-    if (this.server){
+    if (this.server) {
       return await this.server.launchApplication(packageName, activity);
     }
-    let url = `${this.address}launch?package=${packageName}&activity=${activity}&t=${new Date().getTime()}`;
-    let resp = await fetch(url);
-    if (resp.status != 200) {
+    const url = `${this.address}launch?package=${packageName}&activity=${activity}&t=${new Date().getTime()}`;
+    const resp = await fetch(url);
+    if (resp.status !== 200) {
       return Promise.reject(new LauncherResponseError("status code not 200!"));
     }
     return true;
   };
+
   requestPermission = async (name) => {
-    if (this.server){
+    if (this.server) {
       return await this.server.requestPermission(name);
     }
-    let url = `${this.address}request-permission?name=${name}&t=${new Date().getTime()}`;
-    let resp = await fetch(url);
-    if (resp.status != 200) {
+    const url = `${this.address}request-permission?name=${name}&t=${new Date().getTime()}`;
+    const resp = await fetch(url);
+    if (resp.status !== 200) {
       return Promise.reject(new LauncherResponseError("status code not 200!"));
     }
-    return true
+    return true;
   };
+
   startDebugSession = () => {
     this.server = new MockServer();
   };
